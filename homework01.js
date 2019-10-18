0ce89c371966b86cc5b110bfd7aeaaa476b13629
Kasia <kasya.gajdukevich@mail.ru>
2019-10-18 20:23:49 +0300

    работа над ошибками

--- homework01.js
+++ homework01.js
@@ -1,26 +1,13 @@
-var myMsg;
+<script> 
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
- var myMsg:  money = (88);
-console.log(typeof money);
+alert("Hello world!"); 
 
- var myMsg income:  =("50");
-console.log(typeof income);
-console.log(income length);
+console.log(9);
 
-var myMsg addExpenses:  =("17", "20", "40");
-console.log(addExpenses toLowerCase ());
-console.log(addExpenses split (, ));
-myMsg deposit = true;
-
-console.log(typeof deposit);
-
-var myMsg mission: = (90);
-console.log()
-
-var myMsg period = (10);
-
-var  budgetDay =(money/30)
-
-    
-
-             
+</script>

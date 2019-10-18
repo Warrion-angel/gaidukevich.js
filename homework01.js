08e9c424b62c52f8ccd7f170a8dab18f06087343
Kasia <kasya.gajdukevich@mail.ru>
2019-10-18 18:04:40 +0300

    6

--- homework01.js
+++ homework01.js
@@ -1,15 +1,26 @@
-'use strict';
+var myMsg;
 
-let myMsg;
-myMsg  money =
-myMsg income  = 
-myMsg addExpenses  =
-myMsg deposit =
-myMsg mission =
-myMsg period =
+ var myMsg:  money = (88);
+console.log(typeof money);
 
-    alert (Hello!)
+ var myMsg income:  =("50");
+console.log(typeof income);
+console.log(income length);
+
+var myMsg addExpenses:  =("17", "20", "40");
+console.log(addExpenses toLowerCase ());
+console.log(addExpenses split (, ));
+myMsg deposit = true;
+
+console.log(typeof deposit);
+
+var myMsg mission: = (90);
+console.log()
+
+var myMsg period = (10);
+
+var  budgetDay =(money/30)
+
     
-    console.log (Let go);
 
              

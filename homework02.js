35cb4a15fffbaf9fec93d45a6137ac47585da0aa
Kasia <kasya.gajdukevich@mail.ru>
2019-10-19 18:40:16 +0300

    отпр-е

--- homework02.js
+++ homework02.js
@@ -1,15 +1,21 @@
 "use strict";
+
 let money =1200,
+
+
 income= "400",
 addExpenses = "20, 40, 40",
 deposit = false,
 mission = 2000,
-period = 6;
+period = 6,
+budgetDay = 1200/30;
+console.log("Период" + period + "месяцев" + mission + "долларов");
   console.log(typeof money);
   console.log(typeof income);
   console.log( typeof deposit);
   console.log(income.length);
   console.log(addExpenses.toLowerCase);
   console.log(addExpenses.split (, ));
-  console.log()
+  console.log(40);
+  
 

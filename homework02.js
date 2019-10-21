6091ccc30ee6087e0f087063e84ac4d4c967b39f
Kasia <kasya.gajdukevich@mail.ru>
2019-10-21 11:50:35 +0300

    исправление

--- homework02.js
+++ homework02.js
@@ -6,15 +6,15 @@ addExpenses = "20, 40, 40",
 deposit = false,
 mission = 2000,
 period = 6,
-budgetDay = 1200/30;
-console.log("Период " + period + " месяцев.");
-console.log("Цель заработать " + mission +  " долларов");
+budgetDay = money/30;
 console.log(typeof money);
 console.log(typeof income);
 console.log( typeof deposit);
 console.log(income.length);
-console.log(addExpenses.toLowerCase);
+console.log("Период " + period + " месяцев.");
+console.log("Цель заработать " + mission +  " долларов");
+console.log(addExpenses.toLowerCase ());
 console.log(addExpenses.split (", "));
-console.log(40);
+console.log(money/30,money%30);
   
 

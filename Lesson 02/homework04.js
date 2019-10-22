9e25ae62efbac9ad7f6ca9bc466aaeb9beaa15b5
Kasia <kasya.gajdukevich@mail.ru>
2019-10-22 22:51:54 +0300

    комментарий2

---
+++ Lesson 02/homework04.js
@@ -0,0 +1,13 @@
+'use strict';
+let a1 = +prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = +prompt("Во сколько обойдется?", ''),
+    a3 = +prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = +prompt("Во сколько обойдется?", '');
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4; 
+ let budgetMonth = (money - a1 - a3);
+ function getExpensesMonth (budgetMonth){
+     alert ("Ваши расходы в месяц составляют" + budgetMonth)
+ }
+ return (budgetMonth),
+ 
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict';
 let mission = 2000;
+let period = 6;
+let income = "400";
 let money = +prompt ("Ваш месячный доход?", "");
 let addExpenses = prompt ("Перечислите возможные расходы за рассчитываемый период через запятую", "");
 console.log(addExpenses.split (", "));
 
-let deposit = prompt ("Если у вас депозит в банке", "false");
+let deposit = confirm (Boolean ("Если у вас депозит в банке"), "false");
 console.log(typeof money);
 console.log(typeof income);
 console.log( typeof deposit);
+
 let a1 = +prompt("Введите обязательную статью расходов в этом месяце", ''),
     a2 = +prompt("Во сколько обойдется?", ''),
     a3 = +prompt("Введите обязательную статью расходов в этом месяце", ''),
@@ -15,22 +18,22 @@ let a1 = +prompt("Введите обязательную статью расх�
 appData.expenses[a1] = a2;
 appData.expenses[a3] = a4; 
  let budgetMonth = (money - a1 - a3);
- 
- console.log(Mach.ceil (mission/budgetMonth));
+
+ console.log( Mach.ceil (mission/budgetMonth));
  console.log(Math.floor(budgetMonth/30));
 
 
- if(budgetDay >800){
-     alert ("Высокий уровень дохода")
- }
-if(budgetDay 300>800){
-    alert ("Средний уровень дохода")
-}
-if(budgetDay 0>300){
-    alert ("Низкий уровень дохода")
-}
-if (budgetDay <0) {
-    alert ("Что -то пошло не так")
+ if('budgetDay'  > 800) {
+     alert ("Высокий уровень дохода");
+ } 
+  if ('budgetDay' (300 > 800)) {
+    alert ("Средний уровень дохода");
+} 
+ if ('budgetDay' (0 > 300)) {
+    alert ("Низкий уровень дохода");
+}  
+ if ('budgetDay' < 0) {
+         alert ("Что -то пошло не так");
 }
 
 
